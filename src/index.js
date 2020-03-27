cddbb63a36c9a4ac2a8745b510944e5720c33f0a
Tomer S <tomer@shvueli.com>
2020-03-27 11:50:43 -0400

    Only add analytics in production

--- src/index.js
+++ src/index.js
@@ -3,8 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Only track analytics in production
+if (process.env.NODE_ENV === 'production') {
+  const instance = createInstance({
+    urlBase: "https://analytics.brillicity.com/",
+    siteId: 11,
+  });
+
+  ReactDOM.render(
+    <MatomoProvider value={instance}>
+      <App env={process.env.NODE_ENV} />
+    </MatomoProvider>,
+    document.getElementById('root')
+  );
+} else {
+  ReactDOM.render(
+    <App env={process.env.NODE_ENV} />,
+    document.getElementById('root')
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
