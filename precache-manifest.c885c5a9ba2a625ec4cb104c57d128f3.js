8f88560f30f5d5230d122ae39596af31d39f9a82
Tomer S <tomer@shvueli.com>
2020-06-03 18:58:07 -0400

    Updates

---
+++ precache-manifest.c885c5a9ba2a625ec4cb104c57d128f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54374a7a99f8d414cab4b2b39be1c38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e469c92c059ae06d3c81",
+    "url": "/static/css/main.21f32150.chunk.css"
+  },
+  {
+    "revision": "7b13e811592c3f2a6dd2",
+    "url": "/static/js/2.59154562.chunk.js"
+  },
+  {
+    "revision": "598adc37cdd41a78bc7c4daafd7badcb",
+    "url": "/static/js/2.59154562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e469c92c059ae06d3c81",
+    "url": "/static/js/main.6a511cf9.chunk.js"
+  },
+  {
+    "revision": "545bae4590dc51b4bce8",
+    "url": "/static/js/runtime-main.cd010cb3.js"
+  }
+]);
